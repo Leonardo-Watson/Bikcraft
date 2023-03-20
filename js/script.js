f181caf34202eb58411fe222b600fc00a87ba14c
Leonardo Watson <43691685+Lofinho@users.noreply.github.com>
2023-03-20 17:38:32 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,74 @@
+// Ativar links do menu
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add('ativo');
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(parametro);
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocaImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width:950px)').matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocaImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação
+
+
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
